c6ac62f2cb3aee3acde72a38140de0b029b7a6e9
Joel Eze <jl_eze@yahoo.co.uk>
2014-08-20 18:26:08 +0100

    Assignemnt Completion

--- src/com/between/activies/ActivityB.java
+++ src/com/between/activies/ActivityB.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ActivityB extends ActionBarActivity {
@@ -14,18 +15,33 @@ public class ActivityB extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_activity_b);
 		
+		  Intent intent = getIntent();
+	      String name = intent.getStringExtra("name");
+	      String job = intent.getStringExtra("job");
+	      String bool = intent.getStringExtra("boolean");
+	      String amount = intent.getStringExtra("amount");
+	      
+	      TextView tv = (TextView)findViewById(R.id.text);
+	      TextView tv1 = (TextView)findViewById(R.id.textView1);
+	      TextView tv2 = (TextView)findViewById(R.id.textView2);
+	      TextView tv3 = (TextView)findViewById(R.id.textView3);
+	      tv.setText("Name : " +name);
+	      tv1.setText("Job Desc : " +job);
+	      tv2.setText("Boolean : " +bool);
+	      tv3.setText("Amount In Bank : " +amount);
 		
-		Intent i = getIntent();
-		String name = i.getStringExtra("Name");
-		String job = i.getStringExtra("Job");
-        boolean bool = i.getBooleanExtra("BooleanValue",false);
-        Integer amount = i.getIntExtra("AmoutInBank", 1000);
-        
-        
-        Toast.makeText(getApplicationContext(), "Name: " + name, Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), "Job: " + job, Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), "Random Boolean Value: " + bool, Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), "Amount In Bank: " + amount, Toast.LENGTH_LONG).show();
+//		Intent i = getIntent();
+//		
+//		String name = i.getStringExtra("Name");
+//		String job = i.getStringExtra("Job");
+//        boolean bool = i.getBooleanExtra("BooleanValue",false);
+//        Integer amount = i.getIntExtra("AmoutInBank", 1000);
+//        
+//        
+//        Toast.makeText(getApplicationContext(), "Name: " + name, Toast.LENGTH_LONG).show();
+//        Toast.makeText(getApplicationContext(), "Job: " + job, Toast.LENGTH_LONG).show();
+//        Toast.makeText(getApplicationContext(), "Random Boolean Value: " + bool, Toast.LENGTH_LONG).show();
+//        Toast.makeText(getApplicationContext(), "Amount In Bank: " + amount, Toast.LENGTH_LONG).show();
 	}
 
 	@Override
@@ -42,7 +58,7 @@ public class ActivityB extends ActionBarActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
-			Toast.makeText(getApplicationContext(), "Thank You @Cc_Hub", Toast.LENGTH_LONG).show();
+			//Toast.makeText(getApplicationContext(), "Thank You @Cc_Hub", Toast.LENGTH_LONG).show();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
--- src/com/between/activies/MainActivity.java
+++ src/com/between/activies/MainActivity.java
@@ -5,26 +5,54 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
-
+	
+	EditText textBox, textBox1, textBox2, textBox3;
+	Button b;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		b = (Button)findViewById(R.id.button);
+		textBox = (EditText)findViewById(R.id.editText);
+		textBox1 = (EditText)findViewById(R.id.editText1);
+		textBox2 = (EditText)findViewById(R.id.editText2);
+		textBox3 = (EditText)findViewById(R.id.editText3);
+		//Int phoneNo = Int.parseLong(textBox3);
+		
+		b.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(MainActivity.this, ActivityB.class);
+				i.putExtra("name", textBox.getText().toString());
+				i.putExtra("job", textBox1.getText().toString()); 
+				i.putExtra("boolean", textBox2.getText().toString());
+				i.putExtra("amount", textBox3.getText().toString());
+				
+			    startActivity(i);
+			}
+		});
 	}
-	@Override
-	protected void onStart() {
-		// TODO Auto-generated method stub
-		super.onStart();
-		Intent i = new Intent(MainActivity.this, ActivityB.class);
-		i.putExtra("Name", "Caleb");
-		i.putExtra("Job", "Software Developer");
-    	i.putExtra("BooleanValue", true);
-    	i.putExtra("AmoutInBank", 3000);
-    	startActivity(i);
-	}
+//	@Override
+//	protected void onStart() {
+//		// TODO Auto-generated method stub
+//		//super.onStart();
+//		Intent i = new Intent(MainActivity.this, ActivityB.class);
+//		i.putExtra("Name", "Caleb");
+//		i.putExtra("Job", "Software Developer");
+//    	i.putExtra("BooleanValue", true);
+//    	i.putExtra("AmoutInBank", 3000);
+//    	startActivity(i);
+//	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -39,7 +67,7 @@ public class MainActivity extends ActionBarActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
-			Toast.makeText(getApplicationContext(), "Thank You @Cc_Hub", Toast.LENGTH_LONG).show();
+			//Toast.makeText(getApplicationContext(), "Thank You @Cc_Hub", Toast.LENGTH_LONG).show();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
